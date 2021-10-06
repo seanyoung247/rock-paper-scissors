25c1622ebc7c094eecee3f3783b763598c898ab1
Sean Young <seanyoung247@gmail.com>
2021-10-06 16:34:38 +0100

    feat: Adds selector component placeholder

---
+++ src/selector.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Badge from './badge.js';
+
+class Selector extends Component {
+  render() {
+    return (
+      <h3>Test</h3>
+    );
+  }
+}
+
+export default Selector;
