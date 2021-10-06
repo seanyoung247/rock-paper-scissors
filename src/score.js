dcad68bd5114b7d23ce52cd2826bf0216e9e5c88
Sean Young <seanyoung247@gmail.com>
2021-10-06 16:34:21 +0100

    feat: Adds score component placeholder

---
+++ src/score.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Score extends Component {
+  render() {
+    return (
+      <div>
+        <p> Score </p>
+        <p> 00 </p>
+      </div>
+    );
+  }
+}
+
+export default Score;
