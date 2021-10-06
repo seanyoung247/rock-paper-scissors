32da07047b29eb9b69fd791364a5ec5043e8efde
Sean Young <seanyoung247@gmail.com>
2021-10-06 16:33:30 +0100

    feat: Adds initial badge code

---
+++ src/badge.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Badge extends Component {
+  render() {
+    return (
+      <h2>Test</h2>
+    );
+  }
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Header from './header.js';
+import Badge from './badge.js';
+
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
+    <Badge />
   </React.StrictMode>,
   document.getElementById('root')
 );
