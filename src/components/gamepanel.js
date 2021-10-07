f3d0faaaa12c9890989913663de57da90afe37a9
Sean Young <seanyoung247@gmail.com>
2021-10-07 16:29:12 +0100

    refactor: Moves components to their own folder

---
+++ src/components/gamepanel.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './gamepanel.css';
+
+class GamePanel extends Component {
+  render() {
+    return (
+      <div id='test'>
+        <p>Game goes here</p>
+      </div>
+    )
+  }
+}
+
+export default GamePanel;
