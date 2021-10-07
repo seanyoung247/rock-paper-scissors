f8e1b07621bcbb315e247cdbff0d1c793b8e245a
Sean Young <seanyoung247@gmail.com>
2021-10-07 16:29:40 +0100

    refactor: Adds component paths to index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './header.js';
-import Badge from './badge.js';
+import Header from './components/header.js';
+import GamePanel from './components/gamepanel.js';
 
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <Badge />
+    <GamePanel />
   </React.StrictMode>,
   document.getElementById('root')
 );
