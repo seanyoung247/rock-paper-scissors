cc9793ed48e3d924b2f83e8737979a856c3a8285
Sean Young <seanyoung247@gmail.com>
2021-10-08 16:41:52 +0100

    feat: Adds state to score component

--- src/components/score.js
+++ src/components/score.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 
 class Score extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {score: 0};
+  }
   render() {
     return (
       <div>
         <p> Score </p>
-        <p> 00 </p>
+        <p> {this.state.score} </p>
       </div>
     );
   }
