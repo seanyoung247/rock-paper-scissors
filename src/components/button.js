c89fdaf0a0ea5e19621643b44d37c10dda797d7a
seanyoung247 <seanyoung247@gmail.com>
2021-10-12 23:09:26 +0100

    feat: Adds constructor and properties to button

--- src/components/button.js
+++ src/components/button.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.title,
+      clickHandler: props.clickHandler
+    };
+  }
   render() {
     return (
       <button onClick={ this.state.clickHandler }>
+        { this.state.title }
       </button>
     );
   }
