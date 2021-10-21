c740eb448c0124623e31566a21b75b789e1b4387
Sean Young <seanyoung247@gmail.com>
2021-10-21 16:31:09 +0100

    feat: adds initial rules modal code

---
+++ src/components/rulesModal.js
@@ -0,0 +1,22 @@
+import Modal from './modal.js';
+
+class RulesModal extends Modal {
+  constructor(props) {
+    super(props);
+  }
+
+
+}
+
+
+
+
+
+
+// <h2>Rules</h2>
+// <div>
+// </div>
+// <Button
+//   title={<img src="images/icon-close.svg" />}
+//   className="modal-close"
+//   onClick={this.toggleShow} />
