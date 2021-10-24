37e85488204d8feef5382c4766e037b80d95fc0f
seanyoung247 <seanyoung247@gmail.com>
2021-10-24 22:48:25 +0100

    feat: Adds rule image to rulesmodal

--- src/components/rulesmodal.js
+++ src/components/rulesmodal.js
@@ -2,14 +2,21 @@ import Modal from './modal.js';
 import Button from './button.js';
 
 import './rulesmodal.css';
+
 import closeIcon from '../images/icon-close.svg';
+import rulesImage from '../images/image-rules.svg';
 
 class RulesModal extends Modal {
   modalContent() {
     return (
       <div>
-        <h2>Rules</h2>
+        <h2 className="rules-title">Rules</h2>
         <div>
+          <img
+            className="rules-image"
+            src={rulesImage}
+            alt="Paper beats Rock beats Scissors beats paper"
+          />
         </div>
         <Button
           title={<img src={closeIcon} alt="Close" />}
