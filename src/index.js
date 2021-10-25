5f2732c4a458da8d3dfb7ff32300dc61330b2c51
Sean Young <seanyoung247@gmail.com>
2021-10-25 17:13:17 +0100

    refactor: Moves main app code to class component

--- src/index.js
+++ src/index.js
@@ -9,17 +9,38 @@ import Header from './components/header.js';
 import GamePanel from './components/gamepanel.js';
 import RulesModal from './components/rulesmodal.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <header>
-      <Header title={<img src={logo} alt="Rock Paper Sissors" />}/>
-    </header>
-    <section>
-      <GamePanel />
-    </section>
-    <footer>
-      <RulesModal id="rules-modal" title="rules" show={false} />
-    </footer>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  render() {
+    return (
+      <React.StrictMode>
+        <header>
+          <div className="title-bar">
+            <div className="title">
+              <img src={logo} alt="Rock Paper Sissors" />
+            </div>
+            <div className="score-wrapper">
+
+            </div>
+          </div>
+        </header>
+      </React.StrictMode>
+    );
+  }
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <header>
+//       <Header title={<img src={logo} alt="Rock Paper Sissors" />}/>
+//     </header>
+//     <section>
+//       <GamePanel />
+//     </section>
+//     <footer>
+//       <RulesModal id="rules-modal" title="rules" show={false} />
+//     </footer>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
