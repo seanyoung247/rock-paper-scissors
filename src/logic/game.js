bad459f8b5841015ab975ca007cd369afd38a52a
Sean Young <seanyoung247@gmail.com>
2021-10-26 11:13:17 +0100

    feat: Adds random choice generation method

--- src/logic/game.js
+++ src/logic/game.js
@@ -5,13 +5,23 @@ class Game {
   constructor() {
     this.choices = basic;
   }
-  
+  /**
+   * Returns the string value of a given numeric choice
+   */
   getValue(choice) {
     return this.choices[choice];
   }
-  
+  /**
+   * Returns the total number of available choices
+   */
   get choiceCount() {
     return this.choices.length;
+  }
+  /**
+   * Returns a random value within the range of available choices
+   */
+  randomChoice() {
+    return Math.floor(Math.random() * this.choices.length);
   }
 }
 
