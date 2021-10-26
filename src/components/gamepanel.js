c433bacc26c814ae5eaa26d347bfb81f87f53f3e
Sean Young <seanyoung247@gmail.com>
2021-10-26 07:47:24 +0100

    feat: Adds initial game state code to gamepanel

--- src/components/gamepanel.js
+++ src/components/gamepanel.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 import './gamepanel.css';
 
 class GamePanel extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 1
+    }
+  }
   render() {
     return (
       <div id='gamePanel'>
