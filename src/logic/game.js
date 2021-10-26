c4a075eb49547b8939be5ec972effda9ac1e1cf0
Sean Young <seanyoung247@gmail.com>
2021-10-26 11:42:08 +0100

    feat: Adds winner check to game

--- src/logic/game.js
+++ src/logic/game.js
@@ -33,6 +33,26 @@ class Game {
   randomChoice() {
     return Math.floor(Math.random() * this.choices.length);
   }
+  
+  /**
+   * Returns true if the given choice is valid
+   */
+  isValidChoice(choice) {
+    return (choice > 0 && choice < choices.length);
+  }
+  
+  /**
+   * Returns which player is the winner
+   *  @returns: 0 if tie, 1 if player 1, 2 if player 2
+   */
+  checkWinner(p1, p2) {
+    const mod = (a, b, c = a % b) => (c < 0) ? c + b : c;
+    const count = this.choices.length;
+    
+    if (p1 === p2) return 0;
+    if (mod(p1 - p2, count) < count / 2) return 1;
+    else return 2;
+  }
 }
 
 export default Game;
