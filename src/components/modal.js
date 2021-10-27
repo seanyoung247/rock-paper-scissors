9e1bacddc13e1339743c8013361be00427bd5e3d
Sean Young <seanyoung247@gmail.com>
2021-10-27 21:42:36 +0100

    refactor: converts Modal to function component

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
-
+import React, { Fragment } from 'react';
 import './modal.css';
 
-class Modal extends Component {
-    render() {
-      return (
-        <React.Fragment>
-          {
-            this.props.show && (
-              <div className="modal-wrapper">
-                <div className="modal-dialog">
-                  {this.props.children}
-                </div>
-              </div>
-            )
-          }
-        </React.Fragment>
-      );
-    }
+function Modal(props) {
+  return (
+    <Fragment>
+      {
+        props.show && (
+          <div className="modal-wrapper">
+            <div className="modal-dialog">
+              {props.children}
+            </div>
+          </div>
+        )
+      }
+    </Fragment>
+  );
 }
 
 export default Modal;
