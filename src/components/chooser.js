a3bd41bb21038503af283f717c289b831f99d909
Sean Young <seanyoung247@gmail.com>
2021-10-27 21:37:27 +0100

    refactor: Converts Chooser to function component

--- src/components/chooser.js
+++ src/components/chooser.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 
-class Chooser extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.choices.map((data, index)=>(
-          <button key={index}
-            onClick={()=>this.props.choose(this.props.player, index)}>
-            {data}
-          </button>
-        ))}
-      </div>
-    );
-  }
+function Chooser(props) {
+  return (
+    <div>
+      {props.choices.map((data, index)=>(
+        <button key={index}
+          onClick={()=>props.choose(props.player, index)}>
+          {data}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export default Chooser;
