905c3613b5e0cb4f790b679241e719513b2c7548
Sean Young <seanyoung247@gmail.com>
2021-10-27 13:42:08 +0100

    refactor: Removes unneeded player variables

--- src/sections/gamepanel.js
+++ src/sections/gamepanel.js
@@ -6,14 +6,6 @@ import GameUI from '../components/gameui.js';
 import Button from '../components/button.js';
 
 class GamePanel extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      player1: -1,
-      player2: -1
-    };
-  }
-
   render() {
     return (
       <div id='gamePanel'>
