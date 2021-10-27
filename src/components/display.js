6cfbd6d3ab606ec72cad06bc069268c3546124c2
Sean Young <seanyoung247@gmail.com>
2021-10-27 21:39:37 +0100

    refactor: Converts Display to function component

--- src/components/display.js
+++ src/components/display.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react';
 
-class Display extends Component {
-  render() {
-    const game = this.props.game;
-    const winner = game.winner;
-    return (
-      <div>
-        {game.getPlayer(1).choice} {game.getPlayer(2).choice}
-        {winner && (
-          <div>
-            <p>Winner: {winner.name}</p>
-            <button onClick={this.props.reset}>
-              Play Again
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
+function Display(props) {
+  const game = props.game;
+  const winner = game.winner;
+  return (
+    <div>
+      {game.getPlayer(1).choice} {game.getPlayer(2).choice}
+      {winner && (
+        <div>
+          <p>Winner: {winner.name}</p>
+          <button onClick={props.reset}>
+            Play Again
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Display;
