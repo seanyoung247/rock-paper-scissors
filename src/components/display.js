a9b78cd6d07040c886a7e75259f78dc0106311b1
Sean Young <seanyoung247@gmail.com>
2021-10-27 20:53:21 +0100

    feat: Display now outputs choices and winner

--- src/components/display.js
+++ src/components/display.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import Button from './button.js';
 
 class Display extends Component {
   render() {
+    const game = this.props.game;
+    const winner = game.winner;
     return (
       <div>
-        {this.props.player1} {this.props.player2}
+        {game.getPlayer(1).choice} {game.getPlayer(2).choice}
+        {winner && (
+          <div>
+            <p>Winner: {winner.name}</p>
+            <Button onClick={this.props.reset}>
+              Play Again
+            </Button>
+          </div>
+        )}
       </div>
     );
   }
