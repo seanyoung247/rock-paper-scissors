33ffd462475fdfd1f56e058f0a684455d5dcf059
Sean Young <seanyoung247@gmail.com>
2021-10-27 12:59:45 +0100

    refactor: Removes game finite state control from GamePanel

--- src/sections/gamepanel.js
+++ src/sections/gamepanel.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Game from '../logic/game.js';
 
 import './gamepanel.css';
 
@@ -10,16 +9,9 @@ class GamePanel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      step: 1,
       player1: -1,
       player2: -1
     };
-    this.game = new Game();
-    this.setStep = this.setStep.bind(this);
-  }
-
-  setStep(step) {
-    this.setState({step: step});
   }
 
   render() {
