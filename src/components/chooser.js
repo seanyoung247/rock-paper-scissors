aa0eedc20226bd89b351781510991d28cb18b2be
Sean Young <seanyoung247@gmail.com>
2021-10-28 07:28:13 +0100

    feat: adds Initial chooser layout

--- src/components/chooser.js
+++ src/components/chooser.js
@@ -1,13 +1,22 @@
+import React from 'react';
+
+import './chooser.css';
 
 function Chooser(props) {
+  const count = { "--n": props.choices.length };// as React.CSSProperties;
   return (
-    <div>
-      {props.choices.map((data, index)=>(
-        <button key={index}
-          onClick={()=>props.choose(props.player, index)}>
-          {data}
-        </button>
-      ))}
+    <div className="chooser">
+      <ul style={count}>
+        {props.choices.map((data, index)=>(
+          <li key={index}>
+            <button
+              className={`choice-icon ${data}`}
+              onClick={()=>props.choose(props.player, index)}>
+              {data}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
