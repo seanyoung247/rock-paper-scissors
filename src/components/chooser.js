7e855185ab8f0b4b916dac07460b105ff5a39a6c
Sean Young <seanyoung247@gmail.com>
2021-10-28 15:49:48 +0100

    feat: Adds item counter to chooser

--- src/components/chooser.js
+++ src/components/chooser.js
@@ -1,14 +1,12 @@
-import React from 'react';
-
 import './chooser.css';
 
 function Chooser(props) {
-  const count = { "--n": props.choices.length };// as React.CSSProperties;
+  const count = { "--n": props.choices.length };
   return (
     <div className="chooser">
       <ul style={count}>
         {props.choices.map((data, index)=>(
-          <li key={index}>
+          <li key={index} style={{ "--i": index}}>
             <button
               className={`choice-icon ${data}`}
               onClick={()=>props.choose(props.player, index)}>
