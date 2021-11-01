5f95124fa88ff5bf5d7ab062f4b6400d70a43bf3
seanyoung247 <seanyoung247@gmail.com>
2021-11-01 01:15:19 +0000

    feat: Adds player names to choice display

--- src/components/display.js
+++ src/components/display.js
@@ -6,18 +6,24 @@ const messages = ["Tie", "Win", "Lose"];
 function Display(props) {
   const game = props.game;
   const winner = game.winner;
-  const p1 = game.getPlayer(1).choice;
-  const p2 = game.getPlayer(2).choice;
-  const c1 = (p1 >= 0) ? game.getValue(p1) : "empty";
-  const c2 = (p2 >= 0) ? game.getValue(p2) : "empty";
+  const p1 = game.getPlayer(1);
+  const p2 = game.getPlayer(2);
+  const c1 = (p1.choice >= 0) ? game.getValue(p1.choice) : "empty";
+  const c2 = (p2.choice >= 0) ? game.getValue(p2.choice) : "empty";
 
   return (
     <div className="display">
-      <div className="player-one">
-        <div className={`badge ${c1}`}/>
+      <div className="player one">
+        <div className={`badge ${c1}`}>
+          {c1}
+        </div>
+        <p className="name">{p1.name} picked</p>
       </div>
-      <div className="player-two">
-        <div className={`badge ${c2}`}/>
+      <div className="player two">
+        <div className={`badge ${c2}`}>
+          {c2}
+        </div>
+        <p className="name">{p2.name} picked</p>
       </div>
 
       {winner && (
