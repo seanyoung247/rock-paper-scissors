354df898d052f4dd59da9a33c9788f84dc83315b
seanyoung247 <seanyoung247@gmail.com>
2021-11-01 12:31:06 +0000

    feat: Styles win indicator for mobile

--- src/components/display.js
+++ src/components/display.js
@@ -1,11 +1,12 @@
 import './display.css';
 import './badges.css';
 
-const messages = ["Tie", "Win", "Lose"];
+const messages = ["Tied", "Win", "Lose"];
 
 function Display(props) {
   const game = props.game;
   const winner = game.winner;
+  const wId = (winner) ? winner.id : 0;
   const p1 = game.getPlayer(1);
   const p2 = game.getPlayer(2);
   const c1 = (p1.choice >= 0) ? game.getValue(p1.choice) : "empty";
@@ -13,15 +14,19 @@ function Display(props) {
 
   return (
     <div className="display">
-      <div className={`player one ${(winner && winner.id === 1) && 'won'}`}>
-        <div className={`badge ${c1}`}>
-          {c1}
+      <div className="player one">
+        <div className={`badge-container ${(wId === 1) && 'won'}`}>
+          <div className={`badge ${c1}`}>
+            {c1}
+          </div>
         </div>
         <p className="name">{p1.name} picked</p>
       </div>
-      <div className={`player two ${(winner && winner.id === 2) && 'won'}`}>
-        <div className={`badge ${c2}`}>
-          {c2}
+      <div className="player two">
+        <div className={`badge-container ${(wId === 2) && 'won'}`}>
+          <div className={`badge ${c2}`}>
+            {c2}
+          </div>
         </div>
         <p className="name">{p2.name} picked</p>
       </div>
